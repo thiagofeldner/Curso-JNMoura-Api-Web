f4a794c6ca77be994d10adf593421086a51e5dbb
Thiago Feldner <tfeldner@hotmail.com>
2024-05-09 16:27:24 -0300

    Realizado a criação e configuração do respositorio Medico e ajustado o controller Medico

--- web-api/Controllers/MedicosController.cs
+++ web-api/Controllers/MedicosController.cs
@@ -1,5 +1,4 @@
-﻿using System.Data.SqlClient;
-using System.Web.Http;
+﻿using System.Web.Http;
 
 namespace web_api.Controllers
 {
@@ -16,70 +15,28 @@ namespace web_api.Controllers
         [HttpGet]
         public IHttpActionResult Get()
         {
-            return Ok(this.repositorioMedico.GetAll());
+            return Ok(this.repositorioMedico.Select());
         }
 
-        // GET: api/Medicos?crm=123456/BH
+        // GET: api/Medicos/id
         [HttpGet]
-        public IHttpActionResult ObterPorId(string crm)
+        public IHttpActionResult Get(int id)
         {
-            Models.Medico medico = new Models.Medico();
-            
-            using (SqlConnection conn = new SqlConnection())
-            {
-                conn.Open();
+            Models.Medico medico = this.repositorioMedico.Select(id);
 
-                using (SqlCommand cmd = new SqlCommand())
-                {
-                    // cmd.CommandText = $"select id, crm, nome from medico where crm = '{crm}';";
-                    cmd.CommandText = "select id, crm, nome from medico where crm = @crm;";
-                    cmd.Parameters.Add(new SqlParameter("@crm", System.Data.SqlDbType.VarChar)).Value = crm;
-                    cmd.Connection = conn;
-
-                    using (SqlDataReader dr = cmd.ExecuteReader())
-                    {
-                        if (dr.Read())
-                        {
-                            medico.Id = (int)dr["id"];
-                            medico.CRM = dr["crm"].ToString();
-                            medico.Nome = dr["nome"].ToString();
-                        }
-                    }
-                }
-            }
-            if (medico.CRM == "")
+            if (medico is null)
                 return NotFound();
 
             return Ok(medico);
         }
 
-        // GET: api/Medicos/id
+        // GET: api/Medicos?crm=123456/BH
         [HttpGet]
-        public IHttpActionResult ObterPorCRM(int id)
+        public IHttpActionResult Get(string CRM)
         {
-            Models.Medico medico = new Models.Medico();
+            Models.Medico medico = this.repositorioMedico.Select(CRM);
 
-            using (SqlConnection conn = new SqlConnection())
-            {
-                conn.Open();
-
-                using (SqlCommand cmd = new SqlCommand())
-                {
-                    cmd.CommandText = $"select id, crm, nome from medico where id = {id};";
-                    cmd.Connection = conn;
-
-                    using (SqlDataReader dr = cmd.ExecuteReader())
-                    {
-                        if (dr.Read())
-                        {
-                            medico.Id = id;
-                            medico.CRM = dr["crm"].ToString();
-                            medico.Nome = dr["nome"].ToString();
-                        }
-                    }
-                }
-            }
-            if (medico.CRM == "")
+            if (medico is null)
                 return NotFound();
 
             return Ok(medico);
@@ -87,25 +44,9 @@ namespace web_api.Controllers
 
         // POST: api/Medicos        
         [HttpPost]
-        public IHttpActionResult Adicionar(Models.Medico medico)
+        public IHttpActionResult Post(Models.Medico medico)
         {
-            int linhasAfetadas = 0;
-                        
-            using (SqlConnection conn = new SqlConnection())
-            {
-                conn.Open();
-                                
-                using (SqlCommand cmd = new SqlCommand())
-                {
-                    cmd.CommandText = "insert into medico(crm, nome) values(@crm, @nome);";
-                    cmd.Parameters.Add(new SqlParameter("@crm", System.Data.SqlDbType.VarChar)).Value = medico.CRM;
-                    cmd.Parameters.Add(new SqlParameter("@nome", System.Data.SqlDbType.VarChar)).Value = medico.Nome;
-                    cmd.Connection = conn;
-                    linhasAfetadas = cmd.ExecuteNonQuery();
-                }                
-            }
-            
-            if (linhasAfetadas == 0)
+            if (!this.repositorioMedico.Insert(medico))
                 return InternalServerError();
 
             return Ok(medico);
@@ -113,54 +54,22 @@ namespace web_api.Controllers
 
         // PUT: api/Medicos/5
         [HttpPut]
-        public IHttpActionResult Atualizar(int id, Models.Medico medico)
+        public IHttpActionResult Put(int id, Models.Medico medico)
         {
             if (id != medico.Id)
-                return BadRequest("O id da requisição não coincide com o Id do médico.");
+                return BadRequest("O id da requisição não coincide com o id do médico");
 
-            int linhasAfetadas = 0;
-
-            using (SqlConnection conn = new SqlConnection())
-            {
-                conn.Open();
-
-                using (SqlCommand cmd = new SqlCommand())
-                {
-                    cmd.CommandText = "update medico set crm = @crm, nome = @nome where id = @id;";
-                    cmd.Parameters.Add(new SqlParameter("@crm", System.Data.SqlDbType.VarChar)).Value = medico.CRM;
-                    cmd.Parameters.Add(new SqlParameter("@nome", System.Data.SqlDbType.VarChar)).Value = medico.Nome;
-                    cmd.Parameters.Add(new SqlParameter("@id", System.Data.SqlDbType.VarChar)).Value = medico.Id;
-                    cmd.Connection = conn;
-                    linhasAfetadas = cmd.ExecuteNonQuery();
-                }
-            }
-
-            if (linhasAfetadas == 0)
-                return InternalServerError();
+            if (!this.repositorioMedico.Update(medico))
+                return NotFound();
 
             return Ok(medico);
         }
 
         // DELETE: api/Medicos/5
         [HttpDelete]
-        public IHttpActionResult Excluir(int id)
+        public IHttpActionResult Delete(int id)
         {
-            int linhasAfetadas = 0;
-
-            using (SqlConnection conn = new SqlConnection())
-            {
-                conn.Open();
-
-                using (SqlCommand cmd = new SqlCommand())
-                {
-                    cmd.CommandText = "delete from medico where id = @id;";
-                    cmd.Parameters.Add(new SqlParameter("@id", System.Data.SqlDbType.VarChar)).Value = id;
-                    cmd.Connection = conn;
-                    linhasAfetadas = cmd.ExecuteNonQuery();
-                }
-            }
-
-            if (linhasAfetadas == 0)
+            if (!this.repositorioMedico.Delete(id))
                 return NotFound();
 
             return Ok();
--- web-api/Repositories/SQLServer/Medico.cs
+++ web-api/Repositories/SQLServer/Medico.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 
 namespace web_api.Repositories.SQLServer
@@ -12,7 +13,7 @@ namespace web_api.Repositories.SQLServer
             this.connectionString = connectionString;
         }
 
-        public List<Models.Medico> GetAll()
+        public List<Models.Medico> Select()
         {
             List<Models.Medico> medicos = new List<Models.Medico>();
 
@@ -22,8 +23,8 @@ namespace web_api.Repositories.SQLServer
 
                 using (SqlCommand cmd = new SqlCommand())
                 {
-                    cmd.CommandText = $"select id, crm, nome from medico;";
                     cmd.Connection = conn;
+                    cmd.CommandText = $"select id, crm, nome from medico;";
                     using (SqlDataReader dr = cmd.ExecuteReader())
                     {
                         while (dr.Read())
@@ -42,6 +43,127 @@ namespace web_api.Repositories.SQLServer
             return medicos;
         }
 
+        public Models.Medico Select(int id)
+        {
+            Models.Medico medico = null;
+
+            using (SqlConnection conn = new SqlConnection(this.connectionString))
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandText = "select crm, nome from medico where id = @id;";
+                    cmd.Parameters.Add(new SqlParameter("@id", SqlDbType.Int)).Value = id;
+
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        if (dr.Read())
+                        {
+                            medico = new Models.Medico();
+                            medico.Id = id;
+                            medico.CRM = dr["crm"].ToString();
+                            medico.Nome = dr["nome"].ToString();
+                        }
+                    }
+                }
+            }
+
+            return medico;
+        }
+
+        public Models.Medico Select(string CRM)
+        {
+            Models.Medico medico = null;
+
+            using (SqlConnection conn = new SqlConnection(this.connectionString))
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandText = "select id, crm, nome from medico where crm = @crm;";
+                    cmd.Parameters.Add(new SqlParameter("@crm", SqlDbType.VarChar)).Value = CRM;
+
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        if (dr.Read())
+                        {
+                            medico = new Models.Medico();
+                            medico.Id = (int)dr["id"];
+                            medico.CRM = dr["crm"].ToString();
+                            medico.Nome = dr["nome"].ToString();
+                        }
+                    }
+                }
+            }
+            return medico;
+        }
+
+        public bool Insert(Models.Medico medico)
+        {
+            using (SqlConnection conn = new SqlConnection(this.connectionString))
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.CommandText = "insert into medico(crm, nome)Svalues(@crm, @nome); select convert(int,scope_identity());";
+                    cmd.Parameters.Add(new SqlParameter("@crm", SqlDbType.VarChar)).Value = medico.CRM;
+                    cmd.Parameters.Add(new SqlParameter("@nome", SqlDbType.VarChar)).Value = medico.Nome;
+                    cmd.Connection = conn;
+                    medico.Id = (int) cmd.ExecuteScalar();
+                }
+            }
+
+            return medico.Id != 0;
+        }
+
+        public bool Update(Models.Medico medico)
+        {
+            int linhasAfetadas = 0;
+
+            using (SqlConnection conn = new SqlConnection(this.connectionString))
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandText = "update medico set crm = @crm, nome = @nome where id = @id;";
+                    cmd.Parameters.Add(new SqlParameter("@crm", SqlDbType.VarChar)).Value = medico.CRM;
+                    cmd.Parameters.Add(new SqlParameter("@nome", SqlDbType.VarChar)).Value = medico.Nome;
+                    cmd.Parameters.Add(new SqlParameter("@id", SqlDbType.Int)).Value = medico.Id;
+                    
+                    linhasAfetadas = cmd.ExecuteNonQuery();
+                }
+            }
+
+            return linhasAfetadas == 1;
+        }
+
+        public bool Delete(int id)
+        {
+            int linhasAfetadas = 0;
+
+            using (SqlConnection conn = new SqlConnection(this.connectionString))
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandText = "delete from medico where id = @id;";
+                    cmd.Parameters.Add(new SqlParameter("@id", SqlDbType.VarChar)).Value = id;
+
+                    linhasAfetadas = cmd.ExecuteNonQuery();
+                }
+            }
+
+            return linhasAfetadas == 1;
+        }
 
     }
 }
